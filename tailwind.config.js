d5c2df75f7a6788029397293c9a179b4abc1224d
@s.roertgen <sroertgen@gmail.com>
2023-05-10 13:48:08 +0200

    delete colors to suppress warning

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 const colors = require('tailwindcss/colors')
 
+// delete deprecated colors to suppress warning, see https://github.com/tailwindlabs/tailwindcss/issues/4690#issuecomment-1046087220
+delete colors['lightBlue']
+delete colors['warmGray'];
+delete colors['trueGray'];
+delete colors['coolGray'];
+delete colors['blueGray'];
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     colors: {
       ...colors,
+      gray: colors.neutral,
       hbzorange: "#F5821F",
       headlineBlue: "#192271",
       customBlueBg: "#1D2DB9",
