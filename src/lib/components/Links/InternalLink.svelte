8cee45ebfb2501adcce1f2d23d79e23148e3d6a3
@s.roertgen <sroertgen@gmail.com>
2023-12-05 12:57:40 +0100

    Remove scroll into view. Not necessary in new figma design

--- src/lib/components/Links/InternalLink.svelte
+++ src/lib/components/Links/InternalLink.svelte
@@ -5,15 +5,6 @@
 	export let text: string;
 	export let href: string;
 	export let fillBackground: boolean = true;
-
-	function scrollIntoView(event: MouseEvent) {
-		const target = event.target as HTMLAnchorElement;
-		const el = document.querySelector(target.getAttribute('href')!);
-		if (!el) return;
-		el.scrollIntoView({
-			behavior: 'smooth'
-		});
-	}
 </script>
 
-<a {href} on:click={scrollIntoView} class={fillBackground ? classNameBg : classNameNoBg}>{text}</a>
+<a {href} class={fillBackground ? classNameBg : classNameNoBg}>{text}</a>
