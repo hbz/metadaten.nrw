cb36f5557310b8072353a3a1dd5a65f20e8e6a23
@s.roertgen <sroertgen@gmail.com>
2023-12-11 15:51:16 +0100

    Align margin in item boxes. Fixes #39

--- src/lib/components/ExampleBox.svelte
+++ src/lib/components/ExampleBox.svelte
@@ -7,11 +7,13 @@
 <div
 	on:click={() => (expanded = !expanded)}
 	class:bg-gray-200={!selected}
-	class="w-80 min-h-[96px] mt-[10px] mx-auto rounded-lg shadow border border-zinc-300 justify-center items-center cursor-pointer flex flex-col"
+	class="w-80 mx-auto mt-[10px] rounded-lg shadow border border-zinc-300 justify-center items-center cursor-pointer flex flex-col"
 >
-	<p class="text-center text-fuchsia-700 text-base font-semibold leading-normal mx-2">
-		{title}
-	</p>
+	<div class="flex flex-col justify-center min-h-[96px] items-center">
+		<p class="text-center text-fuchsia-700 text-base font-semibold leading-normal mx-2">
+			{title}
+		</p>
+	</div>
 	{#if expanded}
 		<div
 			class="example-links mx-[20px] flex flex-col text-sky-900 text-base font-normal underline leading-normal block"
